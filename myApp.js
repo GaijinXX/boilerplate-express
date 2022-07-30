1e3db21d091401984937b0c092d5904403478b6e
Akhundov Vahid <vaha.az@gmail.com>
2022-07-30 17:13:29 +0200

    Exercise 6 (Response with JSON with env)

--- myApp.js
+++ myApp.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {res.sendFile(__dirname + "/views/index.html")});
 
 app.use('/public', express.static(__dirname + '/public'));
 
-app.get('/json', (req, res) => res.json({"message": process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json"}));
+app.get('/json', (req, res) => res.json({"message": ((process.env.MESSAGE_STYLE === "uppercase") ? "HELLO JSON" : "Hello json")}));
 
 
 
