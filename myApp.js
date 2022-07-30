964e2dda97add43d8118d0a383a33dae5181bad2
Akhundov Vahid <vaha.az@gmail.com>
2022-07-30 20:39:32 +0200

    Exercise 7 (Request Logger)

--- myApp.js
+++ myApp.js
@@ -3,20 +3,13 @@ let app = express();
 
 console.log("Hello World");
 
-function mwLogger (req, res, next) {
-  console.log(`${req.method} ${req.path} - ${req.ip}`);
-  next();
-}
-
-app.use(mwLogger)
+app.use((req, res, next) => {console.log(`${req.method} ${req.path} - ${req.ip}`); next();});
 
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {res.sendFile(__dirname + "/views/index.html")});
 
 app.get('/json', (req, res) => {res.json({"message": ( (process.env.MESSAGE_STYLE === "uppercase") ? "HELLO JSON" : "Hello json")})});
-
-
 
 
 
