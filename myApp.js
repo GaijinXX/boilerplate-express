05df2b7f28969a5eff0df0465e49e56ead6b89c4
Akhundov Vahid <vaha.az@gmail.com>
2022-07-30 20:52:06 +0200

    Exercise 8 (Middleware Chaining)

--- myApp.js
+++ myApp.js
@@ -4,13 +4,20 @@ let app = express();
 
 console.log("Hello World");
 
+app.get('/json', (req, res) => {res.json({"message": ( (process.env.MESSAGE_STYLE === "uppercase") ? "HELLO JSON" : "Hello json")})});
+
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {res.sendFile(__dirname + "/views/index.html")});
 
 app.use((req, res, next) => {console.log(`${req.method} ${req.path} - ${req.ip}`); next();});
 
-app.get('/json', (req, res) => {res.json({"message": ( (process.env.MESSAGE_STYLE === "uppercase") ? "HELLO JSON" : "Hello json")})});
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json({"time": req.time});
+});
 
 
 
