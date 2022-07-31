19f43469b5416fe8b6a49bdbe106b35c240fc185
Akhundov Vahid <vaha.az@gmail.com>
2022-07-31 15:14:32 +0200

    Exercise 10 (Query Parameters)

--- myApp.js
+++ myApp.js
@@ -21,8 +21,11 @@ app.get('/now', (req, res, next) => {
 
 app.get('/:word/echo', (req, res) => {
     res.json({"echo": req.params.word});
-})
+});
 
+app.route('/name').get((req, res) => {
+    res.json({"name": req.query.first + ' ' + req.query.last})
+});
 
 
 
